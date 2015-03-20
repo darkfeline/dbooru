b08d79dd221be6496094c2bf9a15bb5f05cf5630
Allen Li <darkfeline@felesatra.moe>
2015-03-19 23:26:14 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import setup, find_packages
+
+def get_scripts(path):
+    for name in os.listdir(path):
+        yield os.path.join(path, name)
+
+setup(
+    name='dbooru',
+    version='0.1',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    scripts=list(get_scripts('src/bin')),
+
+    author='Allen Li',
+    author_email='darkfeline@felesatra.moe',
+    description='',
+    license='',
+    url='',
+)
