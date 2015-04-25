f82bcc83a31a8730868dc513f3a01940cacfa170
Allen Li <darkfeline@felesatra.moe>
2015-04-25 15:04:40 -0400

    Add license

--- src/dbooru/fuseop.py
+++ src/dbooru/fuseop.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2015  Allen Li
+#
+# This file is part of dbooru.
+#
+# dbooru is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dbooru is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dbooru.  If not, see <http://www.gnu.org/licenses/>.
+
 """dbooru.fuseop
 
 This module defines the FUSE operations used by dbooru.
@@ -36,7 +53,7 @@ class FUSEOp(llfuse.Operations):
         # ROOT_INODE isn't detected
         # pylint: disable=no-member
         self.ino_table = {
-            llfuse.ROOT_INODE: InoTabEntry(RootInodeHandler(), 1),
+            llfuse.ROOT_INODE: InoTabEntry(RootInodeHandler(self.root), 1),
         }
 
     def destroy(self):
--- src/dbooru/handlers/root.py
+++ src/dbooru/handlers/root.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2015  Allen Li
+#
+# This file is part of dbooru.
+#
+# dbooru is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dbooru is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dbooru.  If not, see <http://www.gnu.org/licenses/>.
+
 """dbooru.handlers.raw
 
 This module contains implementations for handling files and inodes using the
@@ -6,6 +23,7 @@ native file system.
 """
 
 import os
+import time
 
 import llfuse
 
@@ -16,14 +34,9 @@ from .base import BaseInodeHandler
 
 class RootInodeHandler(BaseInodeHandler):
 
-    # XXX
-
-    def access(self, mode, ctx):
-        # Everyone can access root inode.  Maybe this should be limited to the
-        # original user?
-        return True
-
-    def getattr(self):
+    def __init__(self, root):
+        stat = do_os(os.statvfs, root)
+        now = time.time()
         attr = llfuse.EntryAttributes()
         # pylint: disable=no-member
         attr.st_ino = llfuse.ROOT_INODE
@@ -31,25 +44,26 @@ class RootInodeHandler(BaseInodeHandler):
         attr.entry_timeout = 300
         attr.attr_timeout = 300
         attr.st_mode
-        attr.st_nlink
-        attr.st_uid
-        attr.st_gid
-        attr.st_rdev
+        attr.st_nlink = 1  # Fix for subdirectories?
+        attr.st_uid = do_os(os.getpid)
+        attr.st_gid = do_os(os.getgid)
         attr.st_size
-        attr.st_blksize = 4096
+        attr.st_blksize = stat.f_bsize
         attr.st_blocks = 1
-        attr.st_atime
-        attr.st_ctime
-        attr.st_mtime
-        return attr
+        attr.st_atime = now
+        attr.st_ctime = now
+        attr.st_mtime = now
+        self.attr = attr
+
+    def access(self, mode, ctx):
+        # Everyone can access root inode.  Maybe this should be limited to the
+        # original user?
+        return True
+
+    def getattr(self):
+        return self.attr
 
     def lookup(self, name):
-        raise NotImplementedError
-
-    def mkdir(self, name, mode, ctx):
-        raise NotImplementedError
-
-    def mknod(self, name, mode, rdev, ctx):
         raise NotImplementedError
 
     def opendir(self):
